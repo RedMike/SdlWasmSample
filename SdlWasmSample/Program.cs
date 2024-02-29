98def36f839c0cb274dcc83f7a8a8e50a0c58885
Mike Roibu <contact@mikeroibu.com>
2024-02-29 17:33:55 +0000

    Add sample game and call from Program

--- SdlWasmSample/Program.cs
+++ SdlWasmSample/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices.JavaScript;
 using SdlWasmSample;
 
@@ -14,6 +15,9 @@ public static partial class Program
     {
         Console.WriteLine("Initial setup of C# side");
 
+        //redirect Debug.WriteLine to Console (Mono uses it for errors)
+        Trace.Listeners.Add(new ConsoleTraceListener(true));
+        
         if (!SetMainLoopFromJs)
         {
             Console.WriteLine("Setting up main loop from C#");
@@ -23,37 +27,39 @@ public static partial class Program
 
     private static bool _firstRun = true;
     private static DateTime _lastLog = DateTime.UnixEpoch;
-    private static SampleSdl? _sample = null;
-
+    // private static SampleSdl? _sample = null;
+    private static SampleGame? _sample = null;
+    
     [JSExport]
     private static void MainLoop()
     {
-        if (_firstRun)
+        try
         {
-            Console.WriteLine("First run of the main loop");
-            _firstRun = false;
-
-            _sample = new SampleSdl($"Test Window {DateTime.UtcNow:s}", 600, 400);
-        }
-
-        var now = DateTime.UtcNow;
-        if ((now - _lastLog).TotalSeconds > 1.0)
-        {
-            _lastLog = now;
-            Console.WriteLine($"Main loop still running at: {now}");
-        }
-
-        if (_sample != null)
-        {
-            if (_sample.MainLoop())
+            if (_firstRun)
             {
-                //sample wants to quit
-                Console.WriteLine("Sample triggered quit");
-                _sample.Dispose();
-                _sample = null;
-                SampleSdl.QuitSdl();
-                //TODO: clear main loop?
+                Console.WriteLine("First run of the main loop");
+                _firstRun = false;
+
+                //_sample = new SampleSdl($"Test Window {DateTime.UtcNow:s}", 600, 400);
+                _sample = new SampleGame();
             }
+
+            var now = DateTime.UtcNow;
+            if ((now - _lastLog).TotalSeconds > 1.0)
+            {
+                _lastLog = now;
+                Console.WriteLine($"Main loop still running at: {now}");
+            }
+
+            if (_sample != null)
+            {
+                _sample.RunOneFrame();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+            throw;
         }
     }
     
---
+++ SdlWasmSample/SampleGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SdlWasmSample;
+
+public class SampleGame : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    
+    public SampleGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        Console.WriteLine($"Game constructor");
+    }
+    
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        Console.WriteLine($"Game Initialize");
+ 
+        base.Initialize();
+    }
+    
+    protected override void LoadContent()
+    {
+        Console.WriteLine($"Game LoadContent");
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+ 
+        // TODO: use this.Content to load your game content here
+    }
+    
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+ 
+        // TODO: Add your update logic here
+ 
+        base.Update(gameTime);
+    }
+    
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+ 
+        // TODO: Add your drawing code here
+ 
+        base.Draw(gameTime);
+    }
+}
